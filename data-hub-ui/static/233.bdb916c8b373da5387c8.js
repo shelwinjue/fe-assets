017cda7d7ba9ac100ca7eea0c62a1081dea8a26d
Shiwen Zhu <zhusw@zhejianglab.org>
2025-01-06 15:21:15 +0800

    feat: data-hub-ui

---
+++ data-hub-ui/static/233.bdb916c8b373da5387c8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_zjlab_fe_data_hub_ui=self.webpackChunk_zjlab_fe_data_hub_ui||[]).push([[233],{1233:(a,e,u)=>{u.r(e),u.d(e,{default:()=>_});const _=".test-drag-area {\r\n  background-color: #000;\r\n}"}}]);
